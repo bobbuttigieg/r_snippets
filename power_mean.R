6961e6b9c1a6d0ee9b3947aa1205a7a6f3927637
Bob Buttigieg <67937409+bobbuttigieg@users.noreply.github.com>
2020-07-20 15:46:18 +1000

    Update power_mean.R

--- power_mean.R
+++ power_mean.R
@@ -1,5 +1,9 @@
 PM <- function(x,p) {  # x gives the set for which the power mean is to be calculated; p gives the power.
+  if (p == 0) { # This provides for situations where the power = 0, to avoid the issue of calculating a geometric mean where a number is raised to a fractional power of (1/0)
+    prod(x)^(1/length(x))
+  } else { # This provides for all other situations, from >-Inf to <Inf
   (1/length(x)*sum(x^p))^(1/p)
+  }
 }
 
 testdata <- c(10,2,19,24,6,23,47,24,54,77) # This is a test dataset, with expected AM of 28.6, HM of 10.01109 and GM of 18.92809
@@ -8,10 +12,9 @@ testdata <- c(10,2,19,24,6,23,47,24,54,77) # This is a test dataset, with expect
 # GM = p0 or p(1/Inf) - The issue here is that the power cannot be *exactly* 0 because the outcome will always be 1 (because anything raised to the power of 0 = 1).
 # HM = p-1
 
-AM(testdata) == mean(testdata) # This confirms (if output=TRUE) that the function AM() is equivalent to the base function mean()
-AM(testdata) # This outputs the arithmetic mean of the vector
+mean(testdata) # This outputs the arithmetic mean of the vector
 PM(testdata,1) # This also outputs the arithmetic mean of the vector
 PM(testdata,(1/Inf-0.000000001)) # This outputs a number that is almost equal to the geometric mean of the vector
+PM(testdata,0) # This outputs the geometric mean of the vector
 PM(testdata,-1) # This outputs the harmonic mean of the vector
-
 rm(testdata) # This removes the testdata
