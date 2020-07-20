00b46f7d368c066603fbc5b980091f49e4f70808
Bob Buttigieg <67937409+bobbuttigieg@users.noreply.github.com>
2020-07-20 22:01:01 +1000

    Update pythagorian_means.R

--- pythagorian_means.R
+++ pythagorian_means.R
@@ -9,3 +9,16 @@ GM <- function(x) {
 HM <- function(x) {
   length(x)/sum(1/x)
 }
+
+AM_vector <- function(x,y) { # This will output a vector of the averages of each
+  (x+y)/2                    # index pair in x and y - (x1+y1)/2, and so on
+}
+
+GM_vector <- function(x,y) {
+  (x*y)^(1/2)
+}
+
+HM_vector <- function(x,y) {
+  2/(1/x+1/y)
+}
+
